e30676811329ce0048cb625ff143d62cb0599edf
IssueZhou <623274311@qq.com>
2015-02-03 21:54:05 +0800

    修改ipad的toolbar适配

--- LittleKid/VC/CropperImgViewController.m
+++ LittleKid/VC/CropperImgViewController.m
@@ -23,7 +23,7 @@
     [super viewDidLoad];
     
     //添加导航栏和完成按钮
-    UINavigationBar *naviBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    UINavigationBar *naviBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
     [self.view addSubview:naviBar];
     
     UINavigationItem *naviItem = [[UINavigationItem alloc] initWithTitle:@"图片裁剪"];
@@ -39,17 +39,6 @@
     editorView.frame = CGRectMake(0, 0, self.view.frame.size.width ,  self.view.frame.size.width);
     editorView.center = self.view.center;
     
-//    //外边框的宽度及颜色
-//    editorView.borderWidth = 1.f;
-//    editorView.borderColor = [UIColor blackColor];
-//    
-//    //截取框的宽度及颜色
-//    editorView.ratioViewBorderWidth = 5.f;
-//    editorView.ratioViewBorderColor = [UIColor orangeColor];
-//    
-//    //截取比例，我这里按正方形1:1截取（可以写成 3./2. 16./9. 4./3.）
-//    editorView.ratio = 1;
-    
     [self.view addSubview:editorView];
 }
 
@@ -62,7 +51,7 @@
     //通过代理回传给上一个界面显示
     [self.delegate passImage:resultImage];
     
-    [self dismissModalViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
